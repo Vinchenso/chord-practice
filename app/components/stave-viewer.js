6aa7506f896964c2789542566b0d8ef02fc84991
Vinchenso Kendall <chensok@gmail.com>
2019-10-19 12:20:19 +0200

    base class working

--- app/components/stave-viewer.js
+++ app/components/stave-viewer.js
@@ -2,27 +2,46 @@ import Component from '@ember/component';
 import Vex from 'vexflow'
 
 export default Component.extend({
- didInsertElement() {
+init() {
     this._super(...arguments);
 
-const VF = Vex.Flow;
-
-var vf = new VF.Factory({
-  renderer: {elementId: 'port', width: 300, height: 200}
-});
-
-var score = vf.EasyScore();
-var system = vf.System();
-
-system.addStave({
-  voices: [score.voice(score.notes(`(B4 A4)/1`))]
-}).addClef('treble');
-
-vf.draw();
-
+    this.set('vf', Vex.Flow);
   },
+  didInsertElement() {
+    this._super(...arguments);
 
-  didRender(){
-    return
+    this.set('renderer', new this.vf.Renderer(this.element, this.vf.Renderer.Backends.SVG));
+    this.renderer.resize(500, 500);
+    this.set('vfContext', this.renderer.getContext());
+    this.vfContext.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+  },
+  didRender() {
+    this._super(...arguments);
+
+    let stave = new this.vf.Stave(10, 40, 400);
+    stave.addClef("bass").addTimeSignature("4/4");
+    stave.setContext(this.vfContext).draw();
+
+    let dataNotes = [
+      {clef: "bass", keys: ["c/2"], duration: "8"},
+      {clef: "bass", keys: ["c/2"], duration: "8"},
+      {clef: "bass", keys: ["d/2"], duration: "q"},
+      {clef: "bass", keys: ["b/2"], duration: "q"},
+      // {clef: "bass", keys: ["b/2"], duration: "q"},
+
+    ];
+    const self = this
+    if(self.notes){
+      dataNotes.push({clef: "bass", keys: ["b/2"], duration: "q"})
+    }
+
+    let notes = dataNotes.map(_note => new this.vf.StaveNote(_note));
+    let voice = new this.vf.Voice({num_beats: 4, beat_value: 4});
+    let formatter = new this.vf.Formatter();
+
+    voice.addTickables(notes);
+    formatter.joinVoices([voice]).format([voice], 400);
+
+    voice.draw(this.vfContext, stave);
   }
 });
