08fa1182c2381398921f74d6ca88de968cf5cecc
Vinchenso Kendall <chensok@gmail.com>
2019-10-22 15:27:05 +0200

    add tailwind

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,14 +1,36 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const isProduction = EmberApp.env() === 'production';
+
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      './app/index.html',
+      './app/templates/**/*.hbs'
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+}
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
- autoImport: {
+    autoImport: {
       webpack: {
         node: {
           fs: 'empty'
         }
+      }
+    },
+    postcssOptions: {
+      compile: {
+        plugins: [
+          require('postcss-import'),
+          require('tailwindcss')('./app/tailwind/config.js'),
+          ...isProduction ? [purgeCSS] : []
+        ]
       }
     }
   });
